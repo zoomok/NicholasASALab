d6fe21c17c7b0b929f4ab8d8e9bccb8382c9fb58
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-04 18:01:00 +1100

    Add files via upload

---
+++ docs/Data Migration_SSIS_vs_Polybase.sql
@@ -0,0 +1,85 @@
+--===========================================================================================================================
+-- Lab 1 : Load DimBigProduct in On-premises to Azure DW using SSIS
+-- Dimension table is light (count = 30,300)
+--===========================================================================================================================
+1. Source table :
+	- Local Database (DESKTOP-E7F3VBA\SQLEXPRESS)
+	- Table		: DimBigProduct
+
+2. Target table	:
+	- Create table in Azure DW
+	- Schema	: Prod
+	- Table 	: DimBigProduct
+	
+3. Create SSIS package
+	- Visual Studio 2019 : Integration Service Project
+	- Project	: DataLoadToAzureSQLDW
+	- Location	: S:\My_ASA\SSIS
+	- Package	: DF Migrate DimBigProduct to Azure
+	- Run
+
+4. Copy dimension table data to Azure SQL DW
+
+--===========================================================================================================================
+-- Lab 2 : Polybase 6 step process
+--===========================================================================================================================
+1. Create a master key
+2. Create a database scoped credential with the storage key
+3. Create an external data source
+4. Create external file format
+5. Create an external table
+6. Load from the external table
+
+--===========================================================================================================================
+-- Lab 3 : Load FactTransactionHistory in On-premises to Azure DW using Polybase
+-- Fact table is heavy (count = 37,605,696)
+--===========================================================================================================================
+1. Export table to flat file
+	- Local database	: SSMS
+	- Task	-> Export data
+	- Destination		: Flat File destination
+	- File name			: s:\My_ASA\FactTransactionHistory.txt
+	- Column names in the first data row : Unchecked
+	- Source transaction : FactTransactionHistory
+	- Run immediately
+	- File created		: 1 GB
+
+2. Create blob storage account
+	- data lake storage	: datalake0318
+	
+3. Upload flat file to blog storage
+	- upload to container(demo)/(folder)FTH
+
+4. Run Polybase 6 steps process
+	- 
+	
+5. Monitor and confirm successful migration
+
+6. Confirm 60 distributions in destination table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
